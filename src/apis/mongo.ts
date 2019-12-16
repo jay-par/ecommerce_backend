25b951a280784029dba1850f7987e54827f27626
Janne Parviainen <janne.j.parviainen@gmail.com>
2019-12-16 21:01:01 +0200

    fixed uun deconstrutered import

--- src/apis/mongo.ts
+++ src/apis/mongo.ts
@@ -1,6 +1,19 @@
-const mongoose = require("mongoose");
-const Product = require("../models/product");
+const mongoose = require('mongoose');
+const { Product } = require('../models/product');
+
+mongoose.connect('mongodb://ecom_user:password123@ds255728.mlab.com:55728/heroku_1kj197vx', {
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('COnencted');
+});
 
 export const getProducts = () => {
-  console.log("Getting those products");
+  console.log('Getting those products');
+  Product.find(function(err: any, products: any) {
+    console.log('products', products);
+  });
 };
